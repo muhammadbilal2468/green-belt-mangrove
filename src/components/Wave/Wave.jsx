63fc0ac5bc7db295e78aeed3b90ac0b15b412f7d
Muhammad Bilal <muhammadbilalll2468@gmail.com>
2024-02-25 14:59:26 +0800

    first commit

--- src/components/Wave/Wave.jsx
+++ src/components/Wave/Wave.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import "./Wave.css";
 
-const Wave = ({ height }) => {
+const Wave = ({ waveType, height }) => {
+  const bgColor =
+    waveType === "air asin"
+      ? "bg-blue-800"
+      : waveType === "air tawar"
+      ? "bg-yellow-700"
+      : "bg-blue-800";
+
   return (
     <div className="content z-10 bg-transparent">
-      <div className="water" style={{ height: `${height}px` }}></div>
+      <div
+        className={`water ${bgColor}`}
+        style={{ height: `${height}px` }}
+      ></div>
     </div>
   );
 };
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -15,6 +15,10 @@ const Home = () => {
     setLand(e.target.value);
   };
 
+  const handleWaveChange = (e) => {
+    setWave(e.target.value);
+  };
+
   const handleMangroveCountChange = (e) => {
     const value = parseInt(e.target.value);
     if (!isNaN(value)) {
@@ -100,14 +104,34 @@ const Home = () => {
           <h2 className="font-bold text-xl text-blue-600 mb-5">Ombak</h2>
           <div class="mb-5">
             <label
-              htmlFor="wave"
+              for="wave"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Jenis Air
+            </label>
+            <select
+              id="wave"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={wave}
+              onChange={handleWaveChange}
+            >
+              <option selected hidden>
+                Pilih Jenis Air---
+              </option>
+              <option value="air asin">Air Asin</option>
+              <option value="air tawar">Air Tawar</option>
+            </select>
+          </div>
+          <div class="mb-5">
+            <label
+              htmlFor="wave-count"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Tinggi Ombak (meter)
             </label>
             <input
               type="number" // Menggunakan input type "number" untuk memastikan input hanya menerima angka
-              id="wave"
+              id="wave-count"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               value={heightWave}
               onChange={handleWaveHeightChange}
@@ -126,7 +150,7 @@ const Home = () => {
           alt=""
           className="absolute top-0 left-0 right-0 bottom-0 z-10"
         />
-        <Wave height={heightWave * 5} />
+        <Wave height={heightWave * 5} waveType={wave} />
 
         {[...Array(mangroveCount)].map((_, index) => (
           <Mangrove key={index} leftPosition={index * 105} />
